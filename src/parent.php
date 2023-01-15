f842ed80c609acc8c9e71f4a82e3a1b885644f97
Meliyana <meliyana128@gmail.com>
2023-01-16 01:25:36 +0700

    Update parent.php

--- src/parent.php
+++ src/parent.php
@@ -2,6 +2,7 @@
 require_once('data.php');
 
 class parentClass {
+    protected $mysqli;
     public function __construct()
     {
         $this->mysqli = new mysqli(SERVER_NAME, USER_NAME, PASSWORD, DB_NAME);
